67edd11cd2136f92c180e2f55ac23e70589fc999
akitorahayashi <atora.has75@gmail.com>
2025-05-28 22:03:31 +0900

    mlmodelcを直接ライブラリに含め、パッケージテストを可能にした

--- Package.swift
+++ Package.swift
@@ -7,6 +7,7 @@ let package = Package(
     name: "scary-cat-screening-kit",
     platforms: [
         .iOS(.v17),
+        .macOS(.v14)
     ],
     products: [
         .library(
@@ -19,8 +20,21 @@ let package = Package(
             name: "ScaryCatScreeningKit",
             dependencies: [],
             path: "Sources",
+            exclude: ["Info.plist"],
             resources: [
-                .process("OvRModels"),
+                .copy("OvRModels/ScaryCatScreeningML_OvR_black_and_white_v22.mlmodelc"),
+                .copy("OvRModels/ScaryCatScreeningML_OvR_human_hands_detected_v22.mlmodelc"),
+                .copy("OvRModels/ScaryCatScreeningML_OvR_mouth_open_v22.mlmodelc"),
+                .copy("OvRModels/ScaryCatScreeningML_OvR_sphynx_v22.mlmodelc")
+            ]
+        ),
+        .testTarget(
+            name: "ScaryCatScreeningKitTests",
+            dependencies: ["ScaryCatScreeningKit"],
+            path: "ScaryCatScreenerTests",
+            exclude: ["Info.plist"],
+            resources: [
+                .process("TestResources")
             ]
         ),
     ],
